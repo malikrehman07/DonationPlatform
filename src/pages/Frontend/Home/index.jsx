807ef302a4e2aa3d984274788cf71b44c7d2caf7
Rehman <malikabdulrehmanshabir@outlook.com>
2026-03-15 22:21:50 +0500

    'update'

--- src/pages/Frontend/Home/index.jsx
+++ src/pages/Frontend/Home/index.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
+import Hero from './Hero'
 
 const Home = () => {
   return (
-    <main className="container">
-      <div className="row">
-        <div className="col">
-          <h1>Welcome to the Home Page</h1>
-          <p>This is the home page content.</p>
-        </div>
-      </div>
+    <main>
+      <Hero />
     </main>
   )
 }
