418d2951ce904dac0bf3b7ca49e5b62d30e84a2d
Rehman <malikabdulrehmanshabir@outlook.com>
2026-03-16 21:48:06 +0500

    'update'

--- src/pages/Frontend/Home/index.jsx
+++ src/pages/Frontend/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero'
+import Companies from './Companies'
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <Companies />
     </main>
   )
 }
